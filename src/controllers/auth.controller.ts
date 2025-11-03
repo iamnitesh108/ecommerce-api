dd5ef709d90061c42eb306351165a6c63c916772
Nitesh <iamnitesh108@gmail.com>
2025-11-03 18:45:52 +0545

    feat(auth): implement login and signup logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,12 +5,29 @@ import {
   SignUpInput,
   SignUpSchema,
 } from '../schema/users';
+import { prismaClient } from '..';
+import { compareSync, hashSync } from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../secrets';
 
 export const signup = async (
   req: Request<{}, {}, SignUpInput>,
   res: Response
 ) => {
   const { name, email, password } = SignUpSchema.parse(req.body);
+  let user = await prismaClient.user.findFirst({ where: { email: email } });
+
+  if (user) {
+    throw Error('User already exists');
+  }
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, 10),
+    },
+  });
+  res.json(user);
 };
 
 export const login = async (
@@ -18,4 +35,19 @@ export const login = async (
   res: Response
 ) => {
   const { email, password } = LoginSchema.parse(req.body);
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (!user) {
+    throw new Error('User not found');
+  }
+  if (!compareSync(password, user.password)) {
+    throw new Error('Incorrect password');
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+    },
+    JWT_SECRET
+  );
+  res.json({ user, token });
 };
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { login } from '../controllers/auth.controller';
+import { signup, login } from '../controllers/auth.controller';
 
 const authRouter: Router = Router();
 
-authRouter.get('/login', login);
+authRouter.post('/signup', signup);
+authRouter.post('/login', login);
 
 export default authRouter;
