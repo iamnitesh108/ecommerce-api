0750554df6aff75f59d6f2289e31752d04fdeb49
Nitesh <iamnitesh108@gmail.com>
2025-11-03 14:04:42 +0545

    feat(auth): scaffold auth route and controller

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import type { Express, Request, Response } from 'express';
 
 import { PrismaClient } from '@prisma/client';
-
+import rootRouter from './routes';
 import { PORT } from './secrets';
 
 const app: Express = express();
 
 app.use(express.json());
+app.use('/api', rootRouter)
 
 export const prismaClient = new PrismaClient({
   log: ['query'],
